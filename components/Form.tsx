04d1f8bf450127fe57bdd7761d8ec38bb77494a8
omarouhra <omarmaroc26@gmail.com>
2022-07-17 16:05:39 +0100

    Form Component 🔥

---
+++ components/Form.tsx
@@ -0,0 +1,131 @@
+import { useSession } from "next-auth/react";
+import React, { Dispatch, SetStateAction, useRef } from "react";
+import Input from "./Input";
+
+type FormType = {
+  getLinks: () => {};
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+};
+
+function Form({ getLinks, setShowModal }: FormType) {
+  // Session Data
+  const { data, status } = useSession();
+  const userId = data?.user?.id;
+
+  // Form refs
+  const linkTitle = useRef(null);
+  const linkUrl = useRef(null);
+  const linkImage = useRef(null);
+  const linkCategory = useRef(null);
+  const linkDescription = useRef(null);
+
+  const createLink = async event => {
+    event.preventDefault();
+
+    try {
+      const responce = await fetch("/api/link", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: linkTitle.current?.value,
+          imageUrl: linkImage.current?.value,
+          url: linkUrl.current?.value,
+          category: linkCategory.current?.value,
+          description: linkDescription.current?.value,
+          userId: userId,
+        }),
+      });
+      if (responce.ok) {
+        getLinks();
+      }
+    } catch (error) {
+      console.log("there was an error submitting", error);
+    }
+  };
+  return (
+    <form
+      onSubmit={event => {
+        createLink(event);
+      }}>
+      <h2 className='font-cal text-2xl mb-6'>Create a New Link</h2>
+      <div className='grid gap-y-5 w-5/6 mx-auto'>
+        <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
+          <span className='pl-5 pr-1'>📌</span>
+          <input
+            className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
+            name='name'
+            required
+            placeholder='Site Name'
+            ref={linkTitle}
+            type='text'
+          />
+        </div>
+        <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
+          <span className='pl-5 pr-1'>🪧</span>
+          <input
+            className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
+            name='Url'
+            required
+            placeholder='Url'
+            ref={linkUrl}
+            type='url'
+          />
+        </div>
+        <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
+          <span className='pl-5 pr-1'>🖼️</span>
+          <input
+            className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
+            name='image'
+            required
+            placeholder='Image'
+            ref={linkImage}
+            type='url'
+          />
+        </div>
+        <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
+          <span className='pl-5 pr-1'>⭐</span>
+          <input
+            className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
+            name='Category'
+            required
+            placeholder='Category'
+            ref={linkCategory}
+            type='text'
+          />
+        </div>
+        <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
+          <span className='pl-5 pr-1'>🖊️</span>
+          <input
+            className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
+            name='Description'
+            required
+            placeholder='Description'
+            ref={linkDescription}
+            type='text'
+          />
+        </div>
+      </div>
+      <div className='flex justify-between items-center mt-10 w-full'>
+        <button
+          type='button'
+          className='w-full px-5 py-5 text-sm text-gray-600 hover:text-black border-t border-gray-300 rounded-bl  focus:outline-none focus:ring-0 transition-all ease-in-out duration-150'
+          onClick={() => {
+            // setError(null);
+            setShowModal(false);
+          }}>
+          CANCEL
+        </button>
+
+        <button
+          type='submit'
+          // disabled={creatingSite || error !== null}
+          onClick={() => setShowModal(false)}
+          className=' bg-white text-gray-600 hover:text-black w-full px-5 py-5 text-sm border-t border-l border-gray-300 rounded-br focus:outline-none focus:ring-0 transition-all ease-in-out duration-150'>
+          CREATE LINK
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ import { useRef, useState } from "react";
 import Modal from "../components/Modal";
 import useSWR from "swr";
 import Button from "../components/Button";
+import Form from "../components/Form";
 
 export default function Home() {
   // Session Data
   const { data, status } = useSession();
 
-  const userId = data?.user.id;
+  const userId = data?.user?.id;
   const router = useRouter();
 
   // App States
@@ -21,13 +22,6 @@ export default function Home() {
     Array<string | object>
   >([]);
   const [pagination, setPagination] = useState<number>(3);
-
-  // Form refs
-  const linkTitle = useRef(null);
-  const linkUrl = useRef(null);
-  const linkImage = useRef(null);
-  const linkCategory = useRef(null);
-  const linkDescription = useRef(null);
 
   // To be replaced with a hook
   if (status === "unauthenticated") {
@@ -49,6 +43,7 @@ export default function Home() {
       console.log("there was an error submitting", error);
     }
   };
+
   const getUsers = async () => {
     try {
       const response = await fetch("/api/user", {
@@ -93,30 +88,6 @@ export default function Home() {
       user.name.toLowerCase().startsWith(userSearch.toLowerCase())
   );
 
-  const createLink = async event => {
-    event.preventDefault();
-
-    try {
-      const responce = await fetch("/api/link", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title: linkTitle.current?.value,
-          imageUrl: linkImage.current?.value,
-          url: linkUrl.current?.value,
-          category: linkCategory.current?.value,
-          description: linkDescription.current?.value,
-          userId: userId,
-        }),
-      });
-      if (responce.ok) {
-        getLinks();
-      }
-    } catch (error) {
-      console.log("there was an error submitting", error);
-    }
-  };
-
   return (
     <div>
       <Head>
@@ -125,91 +96,11 @@ export default function Home() {
       </Head>
 
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        {/* Need to create a form component! */}
-        <form
-          onSubmit={event => {
-            createLink(event);
-          }}
-          className='inline-block w-full max-w-md pt-8 overflow-hidden text-center align-middle transition-all bg-white shadow-xl rounded-lg'>
-          <h2 className='font-cal text-2xl mb-6'>Create a New Link</h2>
-          <div className='grid gap-y-5 w-5/6 mx-auto'>
-            <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
-              <span className='pl-5 pr-1'>📌</span>
-              <input
-                className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
-                name='name'
-                required
-                placeholder='Site Name'
-                ref={linkTitle}
-                type='text'
-              />
-            </div>
-            <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
-              <span className='pl-5 pr-1'>🪧</span>
-              <input
-                className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
-                name='Url'
-                required
-                placeholder='Url'
-                ref={linkUrl}
-                type='url'
-              />
-            </div>
-            <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
-              <span className='pl-5 pr-1'>🖼️</span>
-              <input
-                className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
-                name='image'
-                required
-                placeholder='Image'
-                ref={linkImage}
-                type='url'
-              />
-            </div>
-            <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
-              <span className='pl-5 pr-1'>⭐</span>
-              <input
-                className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
-                name='Category'
-                required
-                placeholder='Category'
-                ref={linkCategory}
-                type='text'
-              />
-            </div>
-            <div className='border border-gray-700 rounded-lg flex flex-start items-center'>
-              <span className='pl-5 pr-1'>🖊️</span>
-              <input
-                className='w-full px-5 py-3 text-gray-700 bg-white border-none focus:outline-none focus:ring-0 rounded-none rounded-r-lg placeholder-gray-400'
-                name='Description'
-                required
-                placeholder='Description'
-                ref={linkDescription}
-                type='text'
-              />
-            </div>
-          </div>
-          <div className='flex justify-between items-center mt-10 w-full'>
-            <button
-              type='button'
-              className='w-full px-5 py-5 text-sm text-gray-600 hover:text-black border-t border-gray-300 rounded-bl  focus:outline-none focus:ring-0 transition-all ease-in-out duration-150'
-              onClick={() => {
-                // setError(null);
-                setShowModal(false);
-              }}>
-              CANCEL
-            </button>
-
-            <button
-              type='submit'
-              // disabled={creatingSite || error !== null}
-              onClick={() => setShowModal(false)}
-              className=' bg-white text-gray-600 hover:text-black w-full px-5 py-5 text-sm border-t border-l border-gray-300 rounded-br focus:outline-none focus:ring-0 transition-all ease-in-out duration-150'>
-              CREATE LINK
-            </button>
-          </div>
-        </form>
+        <div className='inline-block w-full max-w-md pt-8 overflow-hidden text-center align-middle transition-all bg-white shadow-xl rounded-lg'>
+          <Form getLinks={() => getLinks()} setShowModal={setShowModal} />
+        </div>
       </Modal>
+
       <div className='container mx-auto max-w-5xl my-20 flex flex-col items-start justify-center space-y-12 px-5 '>
         <div>
           {data?.user.name && (
